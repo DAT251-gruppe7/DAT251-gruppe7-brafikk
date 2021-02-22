eb86491c44405c934c50e6576e09040fd3cd2fd6
oyjoh <oyvind.bre.joh@gmail.com>
2021-02-22 19:36:05 +0100

    added sample output from API

--- backend/brafikk/views.py
+++ backend/brafikk/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+class TestView(APIView):
+    def get(self, requst, *args, **kwargs):
+        data = {
+            'id': 'Knappetunnelen',
+            'status': 'open'}
+        return Response(data)
+
+
