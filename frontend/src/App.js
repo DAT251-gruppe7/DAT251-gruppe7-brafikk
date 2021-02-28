ffe892fe9906d40690fe8d980f295b4c80c73bc0
Gunnar <Gunnar.Kleiven@student.uib.no>
2021-02-28 16:07:36 +0100

    Attempting to fix heroku nodejs backpack issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// TODO This is just one way to handle CSRF tokens with axios
+// https://dev.to/mdrhmn/deploying-react-django-app-using-heroku-2gfa 
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: {
+        id: "undefined",
+        title: "undefined",
+        status: "undefined"
+      }
+    }
+  }
+
+  testAxios = () => {
+    axios
+      .get("api/brafikks")
+      .then(res => this.setState({ activeItem: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  renderItem = () => {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ID: {this.state.activeItem.id}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          Status: {this.state.activeItem.status}
+        </p>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h1>
+            Brafikk App
+          </h1>
+          <div>
+            {this.testAxios()}
+            {this.renderItem()}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
