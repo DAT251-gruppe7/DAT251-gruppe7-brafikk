04bed306b840a8635e18e3117c8dacdb92a92239
Gunnar <Gunnar.Kleiven@student.uib.no>
2021-03-06 16:55:16 +0100

    Small update to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: {
+        title: "Vikafjellet",
+        status: "Midlertidig stengt på grunn av sterk vind"
+      }
+    }
+  }
+
+  componentDidMount() {
+    /*
+    this.testAxiosPost();
+    this.testAxiosGet();
+    this.renderItem();
+    */
+  }
+
+  /*
+  testAxiosPost = () => {
+    const testData = {
+      title: "Vikafjellet",
+      status: "Midlertidig stengt på grunn av sterk vind"
+    }
+    axios
+      .post("/api/brafikks/", testData)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+    console.log("Done with POST")
+  }
+
+  testAxiosGet = () => {
+    axios
+      .get("/api/brafikks/1")
+      .then(res => this.setState({ activeItem: res.data }))
+      .catch(err => console.log(err));
+    console.log("Done with GET")
+  }
+  */
+
+  renderItem = () => {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Title: {this.state.activeItem.title}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          Status: {this.state.activeItem.status}
+        </p>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h1>
+            Brafikk App
+          </h1>
+          <div>
+            {this.renderItem()}
+          </div>
+        </header>
+      </div >
+    );
+  }
 }
 
 export default App;
