b015bc381ac4f50f22c9a4919f740938b33fa49b
KAR <kar@cdt.no>
2021-03-08 17:52:58 +0100

    test

--- backend/tests.py
+++ backend/tests.py
@@ -1,11 +1,3 @@
 from django.test import TestCase
 
-# Create your tests here.
-def capitalize_string(s):
-    if not isinstance(s, str):
-        raise TypeError('Please provide a string')
-    return s.capitalize()
-
-def test_capitalize_string():
-    assert capitalize_string('test') == 'Test'
 
