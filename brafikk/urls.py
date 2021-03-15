1223168d61bbdd12b28ffc3fc5128b9aabade027
oyjoh <oyvind.bre.joh@gmail.com>
2021-03-15 15:51:04 +0100

    added empty url

--- brafikk/urls.py
+++ brafikk/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include, re_path
 from backend.views import PoiView
+from django.views.generic import TemplateView
 
 # from rest_framework import routers
 
 urlpatterns = [
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/poi/', PoiView.as_view())
 ]
